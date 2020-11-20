57ab1167785cbc2de00f6ac8e11046f57491db1d
David Barreto Vargas <66341198+davidbarretovargas@users.noreply.github.com>
2020-11-20 12:06:05 -0500

    Type on the search bar and check results

--- concept_test_spec.js
+++ concept_test_spec.js
@@ -3,5 +3,8 @@ describe ("Navigation", function()
     it("access URL",function()
     {
         cy.visit('https://www.google.com/')
+        cy.xpath("//*[@name='q']").should("be.enabled").type("Casa Blanca")
+            .type('{enter}')
+        cy.url().should("contain","Casa Blanca")
     })
 })
